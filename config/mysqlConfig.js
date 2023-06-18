29882df37e31d9ab51d9d637f49b483746130413
TonyNogueron <antonio.nogueron03@gmail.com>
2023-06-17 23:32:15 -0600

    updates sql config

--- config/mysqlConfig.js
+++ config/mysqlConfig.js
@@ -1,10 +1,30 @@
 const mysql = require("mysql2");
 
+/*
 const config = {
   host: process.env.DBHOST,
   user: process.env.DBUSER,
   password: process.env.DBPASSWORD,
   database: process.env.DATABASE,
+};
+*/
+
+/*
+const config = {
+  host: "testingclass1004b.ccdundegl4lm.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "",
+  database: "Planty",
+  multipleStatements: true,
+};
+*/
+
+const config = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "Planty",
+  multipleStatements: true,
 };
 
 const connection = mysql.createConnection(config);
@@ -14,8 +34,9 @@ connection.connect((err) => {
     console.log("Error connecting to Db");
     return;
   } else {
+    6;
     console.log("Connection established");
   }
 });
 
-module.exports =  connection;
+module.exports = connection;
